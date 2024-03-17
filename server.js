ed2176b0bc4e51e3dd92f340a921a2304d7f279f
serverPc <linkto898@hotmail.com>
2024-03-18 00:59:58 +0500

    Setup the API

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+var generator = require("generate-password");
+
+const app = express();
+
+const port = 8686;
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log("Server is Running on active port no:" + port);
+});
+
+app.post("/password", (req, res) => {
+  console.log(req.body);
+  let {
+    length,
+    numbers,
+    uppercase,
+    lowercase,
+    symbols,
+    excludeSimilarCharacters,
+    strict,
+  } = req.body;
+
+  res.json(req.body);
+  console.log(length);
+});
+
+app.get("*", (req, res) => {
+  content = {
+    error: "404 NOT Found",
+    developer: "Arbab Hassan",
+    license: "MIT",
+    contact: "https://bit.ly/arbab-hassan",
+  };
+
+  res.json(content);
+});
